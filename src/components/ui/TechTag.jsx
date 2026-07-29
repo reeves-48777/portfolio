077220dc703db4e42e715ca293ed21a99a33841d
Thomas Durand <durandthomas@protonmail.com>
2026-07-30 01:53:02 +0200

    removed dead code and improved readability

--- src/components/ui/TechTag.jsx
+++ src/components/ui/TechTag.jsx
@@ -1,9 +1,8 @@
-import { cn } from "../../utils/cn";
 const TechTag = ({ tech }) => {
   return (
-    <span className={cn("border-2 border-border-base px-3 py-1 text-xs text-content transition-colors hover:border-accent hover:bg-accent hover:font-bold")}>
+    <span className="border-2 border-border-base px-3 py-1 text-xs text-content transition-colors hover:border-accent hover:bg-accent hover:font-bold dark:hover:text-base">
       {tech}
-    </span>
+    </span >
   );
 };
 
