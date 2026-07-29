63bc39725fd2b63d284917c36fdcc376365054f0
Thomas Durand <durandthomas@protonmail.com>
2026-07-30 01:37:42 +0200

    updated rnd lab data

--- src/components/layouts/RndLab.jsx
+++ src/components/layouts/RndLab.jsx
@@ -8,24 +8,35 @@ const RndLab = () => {
       num: "01",
       title: "Rover Network",
       className: "hover:bg-techy hover:border-techy",
-      desc: `Réécriture de l'écosystème web de manière simplifiée et décentralisée
-        - Format de fichier pour les pages statiques (les Shardz)
+      desc: `Implémentation décentralisée du web (work in progress)
+      Le but du projet est de comprendre comment fonctionne le web.
+      Pour cela j'ai décidé de recréer l'écosystème en entier (http, navigateur, js engine, html)
+      Features MVP :
+        - Format de fichier pour les pages statiques (HTML -> Shard)
         - Navigateur (Ravn)
-        - Protocole de communication entre les noeuds du réseau basé sur QUIC (Rover)
-        - Développement d'un framework permettant d'intéragir avec les pages de manière dynamique utilisant un moteur lua (Nest)`,
+        - Protocole de communication entre les noeuds du réseau basé sur QUIC (HTTP -> Rover)
+        - Développement d'un moteur lua (au lieu de javascript) pour dynamiser les pages et intéragir avec le client (V8/SpiderMonkey -> Nest)`,
       techs: ["#RUST", "#TOKIO", "#ICED", "#RATATUI", "#QUIC"]
     },
     {
       num: "02",
       title: "KeyLabs",
-      desc: `Simulation de sons de touches de clavier mécaniques
-      Possibilité de changer les matériaux des différentes parties du clavier (boîtier, plaque, key caps, switchs, mods)`,
+      desc: `Simulation sonore de clavier mécanique (work in progress)
+      Configuration des différentes parties d'un clavier mécanique :
+       - boîtier
+       - plate
+       - mount
+       - pcb
+       - foam
+       - switches
+
+      Le profil sonore du clavier est ensuite généré et les appuis des touches sont interceptés par un daemon pour jouer le son correspondant`,
       techs: ["#RUST", "#WASM", "#REACT", "#R3F", "#WGSL/WebGPU"]
     },
     {
       num: "03",
       title: "Low Carbon Server",
-      desc: `Développement d'outils serveurs et clouds très légers
+      desc: `Développement d'outils serveurs et clouds très légers (work in progress)
       Remplacement d'une suite d'outils cloud:
       - drive/serveur de fichier
       - photo
@@ -34,7 +45,7 @@ const RndLab = () => {
 
       Le but premier est de comprendre comment ces technos fonctionnent réellement mais aussi de faire un geste pour la planète en recyclant un petit et modeste appareil que j'avais sous la main en tirant parti au maximum de la puissance de calcul qu'il peut offrir
       `,
-      techs: ["#RUST", "#WASM", "#REACT", "#R3F", "#WGSL/WebGPU"]
+      techs: ["#RUST", "#TOKIO", "#RATATUI", "#DOCKER", "#AARCH64", "#x86"]
     }
   ];
 
