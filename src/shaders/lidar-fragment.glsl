c924a5203fca236a8a034f82e770f49c96b8e1e1
Thomas Durand <durandthomas@protonmail.com>
2026-07-30 22:44:13 +0200

    removed dead code

--- src/shaders/lidar-fragment.glsl
+++ src/shaders/lidar-fragment.glsl
@@ -13,9 +13,6 @@ void main() {
 
     float scan = smoothstep(0.8, 0.0, dist);
 
-    // float coreScan = smoothstep(0.05, 0.0, dist);
-    // float haloScan = smoothstep(0.3, 0.0, dist);
-
     // Intensité finale
     // float alpha = 0.15 + haloScan * 0.35 + coreScan * 0.5;
     float alpha = 0.2 + scan * 0.8;
